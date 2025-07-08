0aee4948a9590ff2ad45f5ed76059086d86bc742
Artur Celer <acceler945@gmail.com>
2025-07-08 20:58:12 +0200

    Add Navbar and Footer layout components

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='flex w-full justify-center absolute bottom-0 border-t-2 border-(--color-dark-grey) px-[2.5%]'>
+      Taskify 2025
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/layout/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router';
+
+const Navbar = () => {
+  return (
+    <header className='border-b-2 border-(--color-dark-grey) px-[2.5%]'>
+      <nav className='flex justify-between h-20 items-center'>
+        <Link to='/' className='font-bold text-5xl'>
+          Taskify
+        </Link>
+        <Link
+          to='/add'
+          className='bg-(--color-btn-blue) px-8 py-2 rounded-4xl font-semibold text-lg'
+        >
+          New Task +
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
