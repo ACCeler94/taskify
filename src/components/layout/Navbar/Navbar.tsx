cd3372d69572da692d647448d18c4a8a853fa098
Artur Celer <acceler945@gmail.com>
2025-07-09 11:29:18 +0200

    Adjust Navbar heigh

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 const Navbar = () => {
   return (
-    <header className='border-b-2 border-(--color-dark-grey) px-[2.5%]'>
+    <header className='border-b-2 border-(--color-dark-grey) px-[2.5%] h-20'>
       <nav className='flex justify-between h-20 items-center'>
         <Link to='/' className='font-bold text-5xl'>
           Taskify
