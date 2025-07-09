230a0c6347993bb57812f919e9fd013c0109e3cb
Artur Celer <acceler945@gmail.com>
2025-07-09 21:25:51 +0200

    Add redux store and Provider

---
+++ src/store/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import tasksReducer from '../components/features/Tasks/tasksSlice';
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+  },
+});
