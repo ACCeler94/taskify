f64c1cc0df13debfbe4d2bdee871bed21e8d3c73
Artur Celer <acceler945@gmail.com>
2025-07-09 19:37:06 +0200

    Refactor: render columns dynamically

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,31 @@
+import { data } from '../../../API/data';
+import type { Task } from '../../../types/types';
 import TaskColumn from '../../features/TaskColumn/TaskColumn';
 
 const Dashboard = () => {
+  // [TODO - Put data in a redux initial state]
+  const tasks: Task[] = data;
+
+  const columns = [
+    { title: 'TO DO', status: 'todo', dotColor: '#1E88E5' },
+    { title: 'IN PROGRESS', status: 'inProgress', dotColor: '#F5D04C' },
+    { title: 'DONE', status: 'done', dotColor: '#24C57A' },
+  ];
+
   return (
     <main className='flex flex-col'>
       <h1 className='font-bold text-4xl my-5'>Dashboard</h1>
-      <div className='grid grid-cols-3 flex-1'>
-        <TaskColumn title='TO DO' dotColor='#1E88E5' />
-        <TaskColumn title='IN PROGRESS' dotColor='#F5D04C' />
-        <TaskColumn title='DONE' dotColor='#24C57A' />
+      <div className='grid grid-cols-3 flex-1 gap-4'>
+        {columns.map(({ title, status, dotColor }) => (
+          <TaskColumn
+            key={status}
+            title={title}
+            dotColor={dotColor}
+            tasks={tasks.filter((task) => task.status === status)}
+          />
+        ))}
       </div>
     </main>
   );
 };
-
 export default Dashboard;
