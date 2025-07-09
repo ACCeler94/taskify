d6ad772fce351e8190497b37924244ede9504363
Artur Celer <acceler945@gmail.com>
2025-07-09 11:45:50 +0200

    Add TaskColumn component

---
+++ src/components/features/TaskColumn/TaskColumn.tsx
@@ -0,0 +1,29 @@
+const TaskColumn = ({ title, dotColor }: { title: string; dotColor: string }) => {
+  return (
+    <div className='task-column bg-neutral-900 mx-4 rounded-xl p-5'>
+      <h2 className='column-title flex gap-4 items-center text-lg font-semibold'>
+        <svg
+          className='status-dot'
+          width='18px'
+          height='18px'
+          viewBox='0 0 24 24'
+          fill={dotColor}
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+          <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
+          <g id='SVGRepo_iconCarrier'>
+            <path
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z'
+            ></path>
+          </g>
+        </svg>
+        {title}
+      </h2>
+    </div>
+  );
+};
+
+export default TaskColumn;
