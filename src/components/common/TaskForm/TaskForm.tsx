016179b630f5f5264796f49696ef2127e4419283
Artur Celer <acceler945@gmail.com>
2025-07-11 20:06:28 +0200

    Add TaskForm component

---
+++ src/components/common/TaskForm/TaskForm.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import type { NewTask, TaskStatus } from "../../../types/types";
+
+interface TaskFormProps {
+  isNew?: boolean;
+  formTitle: string;
+  taskData?: {
+    title: string;
+    desc: string;
+    status: TaskStatus;
+  };
+  handleSubmit: (
+    e: React.FormEvent<HTMLFormElement>,
+    newTaskData: NewTask,
+  ) => void;
+}
+
+const TaskForm = ({
+  formTitle,
+  taskData,
+  isNew = false,
+  handleSubmit,
+}: TaskFormProps) => {
+  const [title, setTitle] = useState(taskData?.title || "");
+  const [desc, setDesc] = useState(taskData?.desc || "");
+  const [status, setStatus] = useState(taskData?.status || "todo");
+  return (
+    <div className="flex min-h-1/3 w-full max-w-2xl flex-col rounded-xl bg-neutral-800 p-8 text-xl">
+      <h2 className="mb-5 text-3xl font-bold">{formTitle}</h2>
+      <form
+        className="flex w-full flex-col text-xl"
+        onSubmit={(e) => handleSubmit(e, { title, status, desc })}
+      >
+        <input
+          className="mb-4 rounded-lg bg-neutral-600 p-3 text-xl"
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Task title..."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+            e.key === "Enter" && e.preventDefault();
+          }}
+          maxLength={150}
+          required={isNew}
+        />
+        <textarea
+          className="mb-4 min-h-64 resize-none rounded-lg bg-neutral-600 p-3 text-xl"
+          id="desc"
+          name="desc"
+          placeholder="Description..."
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+          maxLength={1000}
+          required={isNew}
+        />
+        <select
+          className="mb-16 rounded-lg bg-neutral-600 p-3 text-xl"
+          id="status"
+          name="status"
+          value={status}
+          onChange={(e) => setStatus(e.target.value as TaskStatus)}
+        >
+          <option value="todo">To Do</option>
+          <option value="inProgress">In Progress</option>
+          <option value="done">Done</option>
+        </select>
+        <button
+          type="submit"
+          className="cursor-pointer rounded-4xl bg-(--color-btn-blue) px-8 py-2 text-lg font-semibold"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default TaskForm;
