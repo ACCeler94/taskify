0561655e0025d117906bfa39f7076b0abd4eb722
Artur Celer <acceler945@gmail.com>
2025-07-11 15:33:07 +0200

    Add TaskDetails page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import Navbar from "./components/layout/Navbar/Navbar";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import TaskDetails from "./pages/TaskDetails/TaskDetails";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="tasks">
+          <Route path=":id" element={<TaskDetails />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ src/pages/TaskDetails/TaskDetails.tsx
@@ -0,0 +1,76 @@
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+import TaskMenu from "../../components/common/TaskMenu/TaskMenu";
+import type { RootState } from "../../types/types";
+
+const TaskDetails = () => {
+  const { id } = useParams();
+  const task = useSelector((state: RootState) =>
+    state.tasks.find((t) => t.id === id),
+  );
+  const statuses = {
+    todo: { title: "TO DO", dotColor: "#1E88E5" },
+    inProgress: { title: "IN PROGRESS", dotColor: "#F5D04C" },
+    done: { title: "DONE", dotColor: "#24C57A" },
+  };
+  const navigate = useNavigate();
+
+  // [TODO] - replace with not found page or redirect to not found page
+  if (!task) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center px-4">
+        <h2 className="text-6xl">404 Task not found...</h2>
+      </div>
+    );
+  }
+
+  const { title, dotColor } = statuses[task.status];
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center px-4">
+      <div className="task-details min-h-1/3 w-full max-w-2xl rounded-xl bg-neutral-800 p-8 text-xl">
+        <div className="task-details__header relative flex items-center justify-between">
+          <h3 className="task-details__title text-2xl font-bold break-words">
+            {task.title}
+          </h3>
+          <TaskMenu taskId={id!} onDelete={() => navigate("/")} />
+        </div>
+
+        <h4 className="task-details__status mt-0.5 flex items-center gap-0.5 text-sm font-light">
+          Status:
+          <svg
+            className="status-dot"
+            width="16px"
+            height="16px"
+            viewBox="0 0 24 24"
+            fill={dotColor}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z"
+              ></path>
+            </g>
+          </svg>
+          {title}
+        </h4>
+
+        <div className="task-details__content mt-6">
+          <p className="task-details__description break-words whitespace-pre-wrap">
+            {task.desc}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskDetails;
