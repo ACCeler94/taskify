ce9c658b9e99765af3d61697ac068c36f75a404b
Artur Celer <acceler945@gmail.com>
2025-07-12 20:02:12 +0200

    Add filterSlice to handle filtering by query and sorting

---
+++ src/components/features/filter/filterSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface InitialState {
+  query: string;
+  sortBy: string;
+  sortingOrder: "asc" | "desc";
+}
+
+const initialState: InitialState = {
+  query: "",
+  sortBy: "createdAt",
+  sortingOrder: "asc",
+};
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    updateSearchQuery: (state, action: PayloadAction<string>) => {
+      state.query = action.payload;
+    },
+    changeSortingOrder: (state, action: PayloadAction<"asc" | "desc">) => {
+      state.sortingOrder = action.payload;
+    },
+  },
+});
+
+export const { updateSearchQuery, changeSortingOrder } = filterSlice.actions;
+export default filterSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "../components/features/tasks/tasksSlice";
+import filterReducer from "../components/features/filter/filterSlice";
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
+    filter: filterReducer,
   },
 });
