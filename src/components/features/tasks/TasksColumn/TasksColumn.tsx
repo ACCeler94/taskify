fb85d70a7156a9b1eea094f4bc7e31a66a674a72
Artur Celer <acceler945@gmail.com>
2025-07-13 11:53:29 +0200

    Add logic for filtering tasks in TasksColumn

--- src/components/features/tasks/TasksColumn/TasksColumn.tsx
+++ src/components/features/tasks/TasksColumn/TasksColumn.tsx
@@ -5,26 +5,33 @@ import { sortTasksByDate } from "../../../../utils/sortTasksByDate";
 import TaskCard from "../TaskCard/TaskCard";
 import { selectTasksByStatus } from "../tasksSelectors";
 
-const TasksColumn = ({
-  title,
-  dotColor,
-  status,
-}: {
+type TasksColumnProps = {
   title: string;
   dotColor: string;
   status: TaskStatus;
-}) => {
+};
+
+const TasksColumn = ({ title, dotColor, status }: TasksColumnProps) => {
   const tasks = useSelector((state: RootState) =>
     selectTasksByStatus(state, status),
   );
   const sortingOrder = useSelector(
     (state: RootState) => state.filter.sortingOrder,
   );
+  const query = useSelector((state: RootState) => state.filter.query);
 
-  const sortedTasks = useMemo(
-    () => sortTasksByDate({ sortingOrder, tasks }),
-    [tasks, sortingOrder],
-  );
+  // Sorting and filtering
+  const displayedTasks = useMemo(() => {
+    const sorted = sortTasksByDate({ sortingOrder, tasks });
+
+    if (!query) return sorted;
+
+    return sorted.filter(
+      (task) =>
+        task.title.toLowerCase().includes(query.toLowerCase()) ||
+        task.desc.toLowerCase().includes(query.toLowerCase()),
+    );
+  }, [tasks, sortingOrder, query]);
 
   return (
     <section className="task-column mx-4 rounded-xl bg-neutral-900 p-5">
@@ -37,25 +44,17 @@ const TasksColumn = ({
           fill={dotColor}
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-          <g
-            id="SVGRepo_tracerCarrier"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></g>
-          <g id="SVGRepo_iconCarrier">
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z"
-            ></path>
-          </g>
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z"
+          />
         </svg>
-        {title} ({tasks.length})
+        {title} ({displayedTasks.length})
       </h2>
 
       <ul className="task-list space-y-2">
-        {sortedTasks.map((task) => (
+        {displayedTasks.map((task) => (
           <li key={task.id}>
             <TaskCard {...task} />
           </li>
